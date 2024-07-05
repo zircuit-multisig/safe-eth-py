f95eb9c49c387f59d82ddb3b064bd2745abbc89b
Leoni Mella <leoni.mella@gmail.com>
2024-07-05 14:38:18 -0300

    feat: add Zircuit Mainnet support

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -323,6 +323,14 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.SHYFT_TESTNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 1984340, "1.3.0+L2"),  # v1.3.0
     ],
+    EthereumNetwork.ZIRCUIT_TESTNET: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 9199713, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 9199719, "1.3.0"),
+    ],
+    EthereumNetwork.ZIRCUIT_MAINNET: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 1, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 1, "1.3.0"),
+    ],
     EthereumNetwork.REI_NETWORK: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 2388036, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 2388042, "1.3.0"),
@@ -1065,6 +1073,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.ZIRCUIT_TESTNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 9199668),  # v1.3.0
+    ],
+    EthereumNetwork.ZIRCUIT_MAINNET: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 131958),  # v1.3.0
     ],
     EthereumNetwork.REI_NETWORK: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 2387999),  # v1.3.0
